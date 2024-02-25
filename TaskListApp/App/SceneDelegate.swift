de2105c6729d53a1a52dbc42a2e68ad6d41addd4
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2024-02-25 22:49:29 +0300

    Add edit and delete task function

--- TaskListApp/App/SceneDelegate.swift
+++ TaskListApp/App/SceneDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private let sessionManager = SessionManager.shared
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -19,8 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        sessionManager.saveContext()
     }
 
 
--- TaskListApp/ViewControllers/TaskListViewController.swift
+++ TaskListApp/ViewControllers/TaskListViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 final class TaskListViewController: UITableViewController {
+    
+    // MARK: - Private Properties
     private var taskList: [ToDoTask] = []
     private let cellID = "task"
     
+    private let sessionManager = SessionManager.shared
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
@@ -18,49 +23,70 @@ final class TaskListViewController: UITableViewController {
         setupNavigationBar()
         fetchData()
     }
-
+    
+    // MARK: - Private Methods
     @objc private func addNewTask() {
-        showAlert(withTitle: "New Task", andMessage: "What do you want to do?")
+        showAlert(
+            withTitle: "New Task",
+            message: "What do you want to do?"
+        ) { [unowned self] taskName in
+            save(taskName)
+        }
     }
     
     private func fetchData() {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let fetchRequest = ToDoTask.fetchRequest()
         
         do {
-            taskList = try appDelegate.persistentContainer.viewContext.fetch(fetchRequest)
+            taskList = try sessionManager.persistentContainer.viewContext.fetch(fetchRequest)
         } catch {
             print(error)
         }
     }
     
-    private func showAlert(withTitle title: String, andMessage message: String) {
+    // MARK: - Private Methods for Alert Controller
+    private func showAlert(
+        withTitle title: String,
+        message: String,
+        text: String? = nil,
+        completion: ((String) -> Void)? = nil
+    ) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let saveAction = UIAlertAction(title: "Save Task", style: .default) { [unowned self] _ in
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
             guard let taskName = alert.textFields?.first?.text, !taskName.isEmpty else { return }
-            save(taskName)
+            completion?(taskName)
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
         alert.addTextField { textField in
             textField.placeholder = "New Task"
+            textField.text = text
         }
+        
         present(alert, animated: true)
     }
     
     private func save(_ taskName: String) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let task = ToDoTask(context: appDelegate.persistentContainer.viewContext)
+        let task = ToDoTask(context: sessionManager.persistentContainer.viewContext)
         task.tittle = taskName
         taskList.append(task)
         
         let indexPath = IndexPath(row: taskList.count - 1, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
         
-        appDelegate.saveContext()
+        sessionManager.saveContext()
     }
-
+    
+    private func update(_ taskName: String, at indexPath: IndexPath) {
+        let task = taskList[indexPath.row]
+        task.tittle = taskName
+        
+        tableView.reloadData()
+        sessionManager.saveContext()
+    }
 }
 
 // MARK: - UITableViewDataSource
@@ -78,6 +104,45 @@ extension TaskListViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showAlert(
+            withTitle: "Edit Task",
+            message: "What do you want to do?",
+            text: taskList[indexPath.row].tittle
+        ) { [unowned self] taskName in
+            update(taskName, at: indexPath)
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension TaskListViewController {
+    override func tableView(
+        _ tableView: UITableView,
+        editingStyleForRowAt indexPath: IndexPath
+    ) -> UITableViewCell.EditingStyle {
+        .delete
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
+    ) {
+        if editingStyle == .delete {
+            let taskToDelete = taskList[indexPath.row]
+            taskList.remove(at: indexPath.row)
+            
+            do {
+                let context = sessionManager.persistentContainer.viewContext
+                context.delete(taskToDelete)
+                try context.save()
+            } catch {
+                print(error)
+            }
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
 
 // MARK: - Setup UI
